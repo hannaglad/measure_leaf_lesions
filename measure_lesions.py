bbd4fcfd28244b660e06a78b15810ec921cd6043
Hanna Glad <hanna.glad@unine.ch>
2023-10-20 13:52:01 +0200

    cleaned up scripts and added parameter explanations

--- measure_lesions.py
+++ measure_lesions.py
@@ -7,10 +7,18 @@ import matplotlib.pyplot as plt
 import progressbar 
 import argparse 
 
-# If you think infected area is missing from the lesions, decrease this value
-# If you think too much non-infected area is being included in the lesions, increase this value
-# Range : 1 - 40
-UPPER_THRESHOLD = 4
+
+# These parameters can be tuned to increase/decrease blurring before we apply the thresholding to detect the lesions. 
+# They aren't included as parameters to the script because they are unlikely to change alot, but if you want to play around with them, feel free.
+# The blurring mainly reduces the artifacts of illumination caused by the scanner beam.
+FIRST_BLUR_KERNEL = (5,5)
+SECOND_BLUR_KERNEL = (9,9)
+BLUR_ITERATIONS = 10 
+GAUSSIAN_BLUR_KERNEL = (11,11)
+
+## The threshold parameter (changeable with --thresh) is the number of bins above the mediam green peak that we want to include as being "normal" tissue. 
+# E.g setting to 0 means that any slight deviation from the main "green" color in the image is sick, wherease setting to 10 means that we are including a large range of green colors as being "normal" tissue.
+
 
 def log_result(lesion_percentage, image_path, logfile="./lesion_measurements.csv"):
 
@@ -23,7 +31,7 @@ def log_result(lesion_percentage, image_path, logfile="./lesion_measurements.csv
         log.write("\n")
 
 
-def detect_lesions(image_path, out_path=0, save=True):
+def detect_lesions(image_path, out_path=0, save=True, upper_threshold=4):
 
     # Read in image 
     image = cv2.imread(image_path, cv2.IMREAD_COLOR)
@@ -79,7 +87,7 @@ def detect_lesions(image_path, out_path=0, save=True):
     thresh_down = bins[green_peak_idx]
 
     # Define upper threshold in terms of bins above the green peak 
-    n_bins_above = UPPER_THRESHOLD
+    n_bins_above = upper_threshold
     thresh_up = bins[green_peak_idx + n_bins_above]
 
     # Use the mask to apply only to the desired areas (erosion for contour, dilation for inside)
@@ -88,17 +96,18 @@ def detect_lesions(image_path, out_path=0, save=True):
 
     ## To reduce the noise comming from the apparent illumination at the top of the screen
     # Perform erosion and apply to the leaf contours
-    blurred_for_leaf = cv2.erode(a_channel_blurred, kernel=(5,5), iterations=10)
+
+    blurred_for_leaf = cv2.erode(a_channel_blurred, kernel=FIRST_BLUR_KERNEL, iterations=BLUR_ITERATIONS)
     
     # To expand real leasions in leaf apply dilation to the inside of the leaf 
-    dilated_in_leaf = cv2.dilate(a_channel_blurred, kernel=(9,9), iterations=10)
+    dilated_in_leaf = cv2.dilate(a_channel_blurred, kernel=SECOND_BLUR_KERNEL, iterations=BLUR_ITERATIONS)
 
     # Apply to the correction locations 
     a_channel_blurred[leaf_indices] = dilated_in_leaf[leaf_indices]
     a_channel_blurred[contour_indices] = blurred_for_leaf[contour_indices]
 
     # Gaussian blur for noise reduction again 
-    a_channel_blurred = cv2.GaussianBlur(a_channel_blurred, (11,11), cv2.BORDER_DEFAULT)
+    a_channel_blurred = cv2.GaussianBlur(a_channel_blurred, GAUSSIAN_BLUR_KERNEL, cv2.BORDER_DEFAULT)
 
     # THRESHOLD 
     th1 = cv2.threshold(a_channel_blurred, thresh_up, 255, cv2.THRESH_BINARY_INV)[1]
@@ -144,29 +153,6 @@ def detect_lesions(image_path, out_path=0, save=True):
 
     return percent
 
-# Write function that just performs the image pre-processing
-def preprocess_image(image_path, save=False, out_path=None):
-     # Read in image 
-    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-
-     # Convert BGR to LAB 
-    lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
-    # Extract A channel (red-green channel)
-    a_channel = lab[:,:,1]
-
-    # Normalize without using the mask (better because takes white which is already the same for each image)
-    unmasked_normalized = cv2.normalize(a_channel, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-    
-    # Make the background white again 
-    unmasked_normalized[boolArr] = 0
-
-    # Save the image 
-    if save:
-        cv2.imwrite(out_path, unmasked_normalized)
-    
-    return unmasked_normalized
-
-
 
 def list_files(input_dir):
     filepaths = [] 
@@ -181,8 +167,8 @@ def list_files(input_dir):
     return filepaths, file_basenames
 
 # Unit test single image 
-def ut(test_path):
-    detect_lesions(test_path, save=False)
+def ut(test_path, thresh=4):
+    detect_lesions(test_path, save=False, upper_threshold=thresh)
 
 
 
@@ -191,11 +177,34 @@ if __name__ == "__main__" :
     
     parser = argparse.ArgumentParser(description = "Detects lesions in images of infected leaves and calculates the percentage of pixels belonging to said lesions")
     parser.add_argument("in_dir", help ="Parent directory of images - should be output from Image pre-processing pipeline")
-    parser.add_argument("outpath", help = "Path to save the lesion overlays")
+    parser.add_argument("--outpath", help = "Path to save the lesion overlays")
+    parser.add_argument("--thresh", help="Set value of threshold for lesion detection. Default is 4. Increase if non-infected area is included in lesions. Decrease if infected area is missing from lesions. Range: 0-38")
     parser.add_argument("--log", help = "Path to save logfile ; Defaults to outpath/lesion_measurements.csv")
+    parser.add_argument("--ut", help = "Run test on single image for debugging", action="store_true")
+
 
     args = parser.parse_args()
+    
 
+    if not args.thresh:
+        upper_threshold = 4
+    else : 
+        upper_threshold = int(args.thresh)
+
+    # If specified, run only a unit test 
+    if args.ut:
+        args.outpath = None
+        print("Running test on single image. Press q to exit")
+        if not os.path.isfile(args.in_dir):
+            print("Please check input path")
+            sys.exit()
+        else:
+            image = args.in_dir
+            ut(image, upper_threshold)
+            sys.exit()
+
+
+    # Otherwise run on a directory 
     if not os.path.isdir(args.in_dir):
         print("Please check input path")
         sys.exit() 
@@ -211,6 +220,8 @@ if __name__ == "__main__" :
     else : 
         log = args.log  
 
+
+
     files, names = list_files(args.in_dir)
 
     # Create progress bar 
@@ -219,7 +230,7 @@ if __name__ == "__main__" :
 
     for i in range(len(files)):
         im_outpath = args.outpath + names[i]
-        percent = detect_lesions(files[i], out_path=im_outpath, save=True)
+        percent = detect_lesions(files[i], out_path=im_outpath, save=True, upper_threshold=upper_threshold)
         log_result(percent, files[i], logfile=log)
         bar.update(i)
     bar.finish()
--- preprocessing_0.4.py
+++ preprocessing_0.4.py
@@ -19,60 +19,90 @@ import time
 import glob
 
 
-DEBUG=False
 
-#logfile = "./logs/log.txt"
 today = datetime.now() 
-#failed_img_dir = "./failed_images"
+
 
 ## USAGE 
 # python preprocessing_0.4.py INPUT_DIR OUTPUT_DIR 
 
-# CLEAN THIS SHIT 
+# NOTES FOR TUNING :
+
+# This script is very messy compared to the script for measuring lesions. It can be greatly improved, but it works and I'm too lazy to fix it. 
+# If you need to use this script, here are some ways you can improve it's performance on your images. 
+
+# 0. Turn on debugging. This will print some information to the terminal that can help you understand what is going on.
+DEBUG=False
+
+# 1. Make sure the reference image is a good representation of the contrast in your images.
+# Just replace the image saved as "reference_image.jpg" with an image that represents your dataset well. 
+
+# 2. If the script fails to detect the reference points, you can try to change the parameters for the reference point detection.
+
+TOP_OF_IMAGE = 500 # The number of pixels from the top of the image to focus on for reference point detection.
+
+# Parameters used in hough circle detection. 
+HOUGH_PARAM1 = 50 
+HOUGH_PARAM2 = 30
+
+# Minimum and maximum fraction of image width to use as radius for reference point DETECTION.
+MIN_RADIUS_FACTOR1 = 0.005
+MAX_RADIUS_FACTOR1 = 0.01
+
+# Minimum and maximum fraction of image width to use as radius for reference point SELECTION.
+MIN_RADIUS_FACTOR2 = 0.0058
+MAX_RADIUS_FACTOR2  = 0.007
+
+# Adjust the expected size of the leaves. This is used to remove the black lines around the leaves.
+min_leaf_area_threshold = 100000.0
+max_leaf_area_threshold = 10000000.0
 
 
-# TO DO :
-# multithread this shit so that is useable.... so... so... so ... slow... 
+# 3. If you have problems decoding the QR code, you can change the parameters for the QR code detection.
+# How many pixels around the qr code to mask along with the QR code 
+QR_EXTRA_BORDER = 5
+QR_DETECTION_ZONE_WIDTH = 400 # How many pixels from the left should the script look for the QR code.
+
+# 4. If you notice the boxes are not being cut correctly (e.g some are too small, others are too big, too much margin included)
+# You can try to modify the following parameters.
+
+X_ADD_RIGHT = 20 # Remove this number of pixels on the right side (removes right margin)
+X_REMOVE_LEFT = 50 # Remove this number of pixels on the left side (removes left margin)
+Y_ADD = 1 # Add this number of pixels vertically to the end of the last box as the first y coordinate for the next box 
+
+# These parameters are used to compare image resolution to the original images the script was designed for. 
+KNOWN_X_LENGTH = 170 # Value in pixels of the known length between the reference circles (based on the original image size) ; used to calculate image resolution so shouldn't change much
+BOX_CONSTANT_WIDTH = 180  # Value in pixels of the width of the boxes (based on the original image size) ; is a constant multiplied by image resolution so shouldn't change much
+BOX_CONSTANT_HEIGHT = 35 # Value in pixels of the height of the boxes (based on the original image size) ; is a constant multiplied by image resolution so shouldn't change much
 
 # ADD MODE FOR MACRO CLEAN UP ::
     # - rewrite extracted leaves and QR codes into original template as to be used as input to existing 
     # IMAGEJ macro 
     # Expected benefits; background removal and contrast normalization!! 
-
 # Maybe add a way to compare total pass rate with an expected pass rate (for example; usr input)
-min_leaf_area_threshold = 100000.0
-max_leaf_area_threshold = 10000000.0
-# Number of individual images per page 
+# Clean Argument parsing 
+
+
+# Number of individual images per page (shouldn't be changed. The script is highly specific for use witht the old macro that takes 8 leaves per page)
 NB_LEAVES_PER_PAGE = 8 
 
-# Expacted size in pixels 
+# Expacted size in pixels (Is actually calculated on real image size, but script is messy so need some definition here.)
 # Aniks original pictures are 9921x14028px full page
 X = 12267
 Y = 17261
 
-#X = 4960
-#Y = 7014
-
 # Parameters for reference circle detection 
-min_radius = int(0.006*X)
-max_radius = int(0.01*X)
-min_radius_2 = int(0.002*X)
-max_radius_2 = int(0.005*X)
+min_radius = int(MIN_RADIUS_FACTOR1*X)
+max_radius = int(MAX_RADIUS_FACTOR1*X)
+min_radius_2 = int(MIN_RADIUS_FACTOR2*X)
+max_radius_2 = int(MAX_RADIUS_FACTOR2*X)
 
-# Paramaters to correctly remove black lines from the outlines of the individual boxes (may need to be optimized per dataset)
-X_ADD_RIGHT = 20
-X_REMOVE_LEFT = 50
-Y_ADD = 1
-KNOWN_X_LENGTH = 170
-BOX_CONSTANT_WIDTH = 180
-BOX_CONSTANT_HEIGHT = 35
-# How many pixels around the qr code to als name = "_".join(decoded[:-1])o mask
-QR_EXTRA_BORDER = 5
 
 # FILE THAT CONTAINS ISOLATE NAMES FOR MAPPING
-name_map="./names_to_qr_codes.csv"
+name_map="./names_to_qr_codes.csv" # This is an optional file that you can use to map the decoded QR codes to your desired isolate names. If it doesn't exist, images will simply be saved as the decoded QRs
+
 # SET REFERENCE IMAGE TO FIT CONTRAST
-reference_image_path = "./reference_image.jpg"
+reference_image_path = "./reference_image.jpg" # This image is good for most datasets, but if you change it might work better. This image should be a good representation of the contrast in your images.
 
 ## Utility functions ##
 def log(string, logfile):
@@ -100,29 +130,32 @@ def display_all_contours(contours, image):
     copy = image.copy() 
     for i in range(len(contours)):
         cv2.drawContours(copy, contours[i], -1, (255, 0, 0), 3)
-    show(copy)
     return 0 
 
 
 ## Processing functions ##
-def detect_ref_points(scaled, show_z=True, radius_params=None):
+def detect_ref_points(scaled, show_z=False, radius_params=None):
+
+    # Set show_z to true if you want to see where the reference points are detected
 
     if radius_params:
         if DEBUG:
+            print("Printing the parameters used to detect reference points in the following order :")
+            print("min radius 1, max radius 1, min radius 2, max radius 2")
             print(radius_params)
         min_radius, max_radius, min_radius_2, max_radius_2 = radius_params
 
     failed = False
     ## Detect reference points at the top of the page 
-    #top = 300
-    top = 500
+    top = TOP_OF_IMAGE
 
     top_of_image = scaled[:top, :, :] # Focus on top of the image
     top_grayscale = cv2.cvtColor(top_of_image, cv2.COLOR_BGR2GRAY) # Convert to grayscale
     top_grayscale = cv2.blur(top_grayscale, (3,3)) # Blur to remove noise 
 
-    circles = cv2.HoughCircles(top_grayscale, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=min_radius, maxRadius=max_radius) # Detect circles
+    circles = cv2.HoughCircles(top_grayscale, cv2.HOUGH_GRADIENT, 1, 20, param1=HOUGH_PARAM1, param2=HOUGH_PARAM2, minRadius=min_radius, maxRadius=max_radius) # Detect circles
     if DEBUG:
+        print("Printing the coordinates of ALL circles detected in the following order : [x, y, radius]")
         print(circles)
     # If less than 2 circles detected, save imag e to failed circles folder 
     if circles is None or circles.shape[1] < 2 :
@@ -132,8 +165,8 @@ def detect_ref_points(scaled, show_z=True, radius_params=None):
         circles = np.round(circles[0, :].astype("int"))
         xlist = []
         ylist = []
-        if show_z:
-            copy = top_of_image.copy() 
+        rlist = []
+        copy = top_of_image.copy()
         
         for x, y, r in circles:
             # Use the radius of the circle to make sure we have detected the correct points
@@ -141,16 +174,32 @@ def detect_ref_points(scaled, show_z=True, radius_params=None):
             if r>= min_radius_2 and r<=max_radius_2:
                 xlist.append(x)
                 ylist.append(y)
+                rlist.append(r)
+                
                 if show_z:
                     cv2.circle(copy, (x,y),r, (0, 255, 0), 4)
         if show_z:
             show(copy)
         
-        # If more than two circles detected, save to failed circles folder 
+ 
+        # If less than two circles detected, save to failed circles folder 
         if len(xlist) < 2 or len(ylist) < 2:
-            print([(x,y,r) for x,y,r in circles])
+            print("Less than two reference points where dected in the image after correction.")
+            if DEBUG:
+                print("Printing the coordinates of SELECTED reference points detected in the following order : [x, y, radius]")
+                for i in range(len(xlist)):
+                    print(xlist[i], ylist[i], rlist[i])
+
             failed = True 
         
+        if len(xlist) > 2 or len(ylist) > 2:
+            print("More than two reference points where dected in the image after correction.")
+            if DEBUG:
+                print("Printing the coordinates of SELECTED reference points detected in the following order : [x, y, radius]")
+                for i in range(len(xlist)):
+                    print(xlist[i], ylist[i], rlist[i])
+            failed = True
+                    
          # Sort by x-coordinate so we don't acidently invert the angle for straightening
         elif xlist[0] > xlist[1]:
             tmpx = xlist[0]
@@ -184,16 +233,19 @@ def scale_intensity(target_grayscale):
     target_rescaled = img_as_ubyte(target_rescaled)
     return target_rescaled
 
-def detect_QR(grayscale, name_map=None, show_zone=False):
+def detect_QR(grayscale, name_map=None, show_zone=False, format=1):
     '''
     Detects QR code in a box
     Input :
         grayscale : grayscale box
         name_nap = file containing alternative names for each isolate; to ensure consistent naming
+        show_zone = whether or not to show the detection zone
+        format = Format of the labels. If format == 1; then the format follows naming that ANIK used (eg. isolate_host_replicate). If format == 2 then the label formatting doesn't matter and all images will be saved to a unique directory
+
     '''
 
-    # REDUCE THE SEARCH SPACE ; VASTLY IMPROVES RESULT
-    detection_zone = grayscale[:, 0:400]
+    # REDUCE THE SEARCH SPACE TOP TOP OF PAGE; VASTLY IMPROVES RESULT
+    detection_zone = grayscale[:, 0:QR_DETECTION_ZONE_WIDTH]
 
     if show_zone:
         show(detection_zone)
@@ -209,30 +261,43 @@ def detect_QR(grayscale, name_map=None, show_zone=False):
         decoded  = decoded.strip("b'")
         decoded = decoded.split("_")
 
-        host = decoded[-1]
-        test = decoded[2]
+        if DEBUG:
+            print("Printing the decoded QR output before parsing")
+            print(decoded)
 
-        if "leaf" in test:
-            replicate = "_".join(decoded[2:4])
-            search_string = "_".join(decoded[0:2])
-        else:
-            replicate = "_".join(decoded[3:5])
-            search_string = "_".join(decoded[0:3])
+        if format == 1:
 
-        if name_map :
-            with open(name_map, 'r') as map: # Search for corresponding name in name_map file
-                for line in map.readlines():
-                    l = line.strip().split(',') 
-                    if search_string in l[1]: # If found in name map, take the correct name 
-                        name=str(l[0])
-                        break
-                    else:
-                        name = "_".join(decoded[:-1])
+            host = decoded[-1]
+            test = decoded[2]
+
+            if "leaf" in test:
+                replicate = "_".join(decoded[2:4])
+                search_string = "_".join(decoded[0:2])
+            else:
+                replicate = "_".join(decoded[3:5])
+                search_string = "_".join(decoded[0:3])
+
+            if name_map :
+                with open(name_map, 'r') as map: # Search for corresponding name in name_map file
+                    for line in map.readlines():
+                        l = line.strip().split(',') 
+                        if search_string in l[1]: # If found in name map, take the correct name 
+                            name=str(l[0])
+                            break
+                        else:
+                            name = "_".join(decoded[:-1])
+    
+            output = [host, name, replicate, rect]
+        
         else : 
-            name = "_".join(decoded[:-1])
-                   
-        return [host, name, replicate, rect]
-
+            name = "_".join(decoded)
+            output = [name, name, name, rect]
+        
+        if DEBUG:
+            print("Printing the decoded QR output after parsing")
+            print([output])
+        return output
+       
     else:
         return None
 
@@ -246,7 +311,7 @@ def log_non_fatal(failed_indexes, image_name, logfile, error_code):
     
 
 
-def process_image(image_path, logfile, name_map=None, show_ref_points=False, show_QR=True, show_contours=True):
+def process_image(image_path, logfile, name_map=None, show_ref_points=False, show_QR=False, show_contours=False, format=1):
     '''
     Applies preprocessing pipeline to A4 scan of wheat leaves 
     Input :  RAW A4 scan of 8 leaves
@@ -264,13 +329,15 @@ def process_image(image_path, logfile, name_map=None, show_ref_points=False, sho
     # Get size and radius for reference points
     X = np.shape(original_image)[1]
     Y = np.shape(original_image)[0]
-    min_radius = int(0.006*X)-1
-    max_radius = int(0.01*X)+1
-    min_radius_2 = int(0.002*X)
-    max_radius_2 = int(0.007*X)
+
+    min_radius = int(MIN_RADIUS_FACTOR1*X)-1
+    max_radius = int(MAX_RADIUS_FACTOR1*X)+1
+    min_radius_2 = int(MIN_RADIUS_FACTOR2*X)
+    max_radius_2 = int(MAX_RADIUS_FACTOR2*X)
     radius_params = [min_radius, max_radius, min_radius_2, max_radius_2]
 
     scaled = cv2.resize(original_image, (int(X/2), int(Y/2)), interpolation=cv2.INTER_AREA) # Downscale by 1/2
+
     ref_points = detect_ref_points(scaled, show_z=show_ref_points, radius_params=radius_params) # Get ref points from top 
     
     # Check if ref point detection has worked 
@@ -312,6 +379,9 @@ def process_image(image_path, logfile, name_map=None, show_ref_points=False, sho
 
             # Crop
             cropped = rotated[:, x1+X_ADD_RIGHT:x2-X_REMOVE_LEFT, :]
+
+            # Uncomment this line to see the cropped image (usefull for testing whether page margins are removed correctly)
+            # show(cropped)
        
             # Calculate the resolution of the image
             observed_x_length = np.sqrt((x2-x1)**2)+((y2-y1)**2)
@@ -329,8 +399,8 @@ def process_image(image_path, logfile, name_map=None, show_ref_points=False, sho
                 # Crop the box
                 current_box = cropped[ypos:yend, :, :]
                 
-                if i == 0:
-                 show(current_box)
+                # Uncomment this line to see the cropped box (usefull for testing whether boxes are cropped correctly)
+                #show(current_box)
                 
                 # Add to current ypos to avoid taking the black outline in the next box
                 ypos = yend+Y_ADD
@@ -342,7 +412,7 @@ def process_image(image_path, logfile, name_map=None, show_ref_points=False, sho
                 except : 
                     raise SystemExit("Issue in grayscale conversion on {}. Exiting.".format(img_name))
                 
-                QR_output = detect_QR(current_box_grayscale, name_map, show_QR)
+                QR_output = detect_QR(current_box_grayscale, name_map, show_QR, format=format)
                 
                 if not isinstance(QR_output, list):
                     cv2.imwrite(failed_img_dir+"/QR/+img_name+index_"+str(i)+".png", current_box)
@@ -431,7 +501,7 @@ def process_image(image_path, logfile, name_map=None, show_ref_points=False, sho
 
 
 def unit_test(image_path):
-    process_image(image_path, show_ref_points=True, show_QR=True, show_contours=True, logfile="./testlog")
+    process_image(image_path, show_ref_points=True, show_QR=True, show_contours=True, logfile="./testlog", format=1)
 
 
 '''
@@ -465,6 +535,12 @@ if __name__ == "__main__":
         output_dir = sys.argv[2]
     except IndexError:
         raise SystemExit("No output directory specified")
+    
+    try : 
+        FORMAT = int(sys.argv[3])
+    except IndexError:
+        FORMAT = 1
+        print("Label format not specified. Assuming format 1. To use format 2, specify 2 as third argument")
 
     # Correct outpath if missing slash
     if not output_dir.endswith("/"):
@@ -526,6 +602,7 @@ if __name__ == "__main__":
     print("Input folder is : {}".format(input_dir))
     print("Parent output folder is : {}".format(output_dir))
     print("Failed images are available in : {}".format(failed_img_dir))
+    print("Images that fail at a specific step are saved in subfolders representing that step. This can help you debug any issues.")
     print("-------------------------------------------")
      
     log("**** New Run on {} ****".format(today), logfile)
@@ -537,7 +614,7 @@ if __name__ == "__main__":
             img_start = time.perf_counter() 
             full_seen += 1 
             filepath = path.join(input_dir, file)
-            boxlist = process_image(filepath, logfile, show_ref_points=False, show_QR=False, show_contours=False)
+            boxlist = process_image(filepath, logfile, show_ref_points=False, show_QR=False, show_contours=False, format=FORMAT)
         
             if len(boxlist) == 0:
                 print("{} failed.".format(file))
@@ -554,8 +631,11 @@ if __name__ == "__main__":
                         mkdir(final_output_dir)
                     except FileExistsError :
                         pass
-
-                    outname = str(final_output_dir+name+replicate+".jpg")
+                        
+                    if name == replicate:
+                        outname = str(final_output_dir+name+".jpg")
+                    else : 
+                        outname = str(final_output_dir+name+replicate+".jpg")
                     cv2.imwrite(outname, img)
                     passed_boxes +=1 
             img_end = time.perf_counter()
